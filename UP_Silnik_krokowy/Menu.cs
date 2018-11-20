319d6abec027aec61bf42f6172181f038fca13ad
Ireneusz Żwirek <zwirek.ireneusz@gmail.com>
2018-11-20 17:42:52 +0100

    Implement all functionalities; State after labs

---
+++ UP_Silnik_krokowy/Menu.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UP_Silnik_krokowy
+{
+    class Menu
+    {
+        public void MainMenu()
+        {
+            Console.WriteLine("*** MENU GLOWNE ***");
+            Console.WriteLine("1. Obroc w prawo");
+            Console.WriteLine("2. Obroc w lewo");
+        }
+    }
+}
