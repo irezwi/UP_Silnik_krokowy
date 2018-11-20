afe735f7bdb13e4486f7deab971a70fb53f3bba0
Ireneusz Żwirek <zwirek.ireneusz@gmail.com>
2018-11-20 18:06:22 +0100

    Final version

---
+++ UP_Silnik_krokowy/ConnectionHandler.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using FTD2XX_NET;
+using System.Threading;
+
+namespace UP_Silnik_krokowy
+{
+    class ConnectionHandler
+    {
+        public FTDI Device { get; set; }
+        public FTDI.FT_STATUS DeviceStatus { get; set; }
+        public UInt32 WrittenBytesCount = 0;
+        public byte[] stepRightBytes = { 0x01, 0x08, 0x02, 0x04 };
+        public byte[] stepLeftBytes = { 0x04, 0x02, 0x08, 0x01 };
+        public byte[] stepLeftDownBytes = { 0x10, 0x80, 0x20, 0x40 };
+        public byte[] stepRightDownBytes = { 0x40, 0x20, 0x80, 0x10 };
+        public byte[] disconnectBytes = { 0x00 };
+        public int index = 0;
+
+        public void Connect()
+        {
+            UInt32 deviceCount = 0;
+            Device = new FTDI();
+            Device.GetNumberOfDevices(ref deviceCount);
+            FTDI.FT_DEVICE_INFO_NODE[] deviceList = new FTDI.FT_DEVICE_INFO_NODE[deviceCount];
+            Device.GetDeviceList(deviceList);
+            Console.WriteLine("Device count: " + deviceList.Length);
+            Device.OpenBySerialNumber(deviceList[0].SerialNumber);
+            Device.SetBitMode(0xff, 1);
+            foreach (FTDI.FT_DEVICE_INFO_NODE d in deviceList)
+            {
+                Console.WriteLine("Nazwa urządzenia: " + d.Description);
+                Console.WriteLine("Serial number urzadzenia: " + d.SerialNumber);
+            }
+        }
+
+        public void Disconnect()
+        {
+            WrittenBytesCount = 0;
+            Int32 bytesToWrite = 1;
+            Device.Write(disconnectBytes, bytesToWrite, ref WrittenBytesCount);
+        }
+
+        public void Step(int count, int speed, byte[] bytes)
+        {
+            WrittenBytesCount = 0;
+            Int32 bytesToWrite = 1;
+            for (int i = 0; i < count; i++)
+            {
+                byte[] x = { bytes[index % 4] };
+                Device.Write(x, bytesToWrite, ref WrittenBytesCount);
+                Thread.Sleep(speed);
+                index++;
+            }
+        }
+    }
+}
--- UP_Silnik_krokowy/Menu.cs
+++ UP_Silnik_krokowy/Menu.cs
@@ -1,18 +1,83 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace UP_Silnik_krokowy
 {
     class Menu
     {
-        public void MainMenu()
+        ConnectionHandler ch = new ConnectionHandler();
+
+        public void ShowMenu()
         {
-            Console.WriteLine("*** MENU GLOWNE ***");
-            Console.WriteLine("1. Obroc w prawo");
-            Console.WriteLine("2. Obroc w lewo");
+            Console.WriteLine("1. Obrot w prawo o ilosc krokow");
+            Console.WriteLine("2. Obrot w lewo o ilosc krokow");
+            Console.WriteLine("3. Obrot w prawo o kat");
+            Console.WriteLine("4. Obrot w lewo o kat");
+            Console.WriteLine("5. Obrot lewej w dol o ilosc krokow:");
+            Console.WriteLine("6. Obrot prawej w dol o ilosc krokow:");
+            Console.WriteLine("7. Obrot lewej w dol o kat:");
+            Console.WriteLine("8. Obrot prawej w dol o kat:");
+            var input = int.Parse(Console.ReadLine());
+            if (input == 1)
+            {
+                Console.WriteLine("Ile krokow w prawo wykonac: ");
+                var inputStepsCount = int.Parse(Console.ReadLine());
+                ch.Step(inputStepsCount, 100, ch.stepRightBytes);
+                ch.Disconnect();
+            }
+            else if (input == 2)
+            {
+                Console.WriteLine("Ile krokow w lewo wykonac: ");
+                var inputStepsCount = int.Parse(Console.ReadLine());
+                ch.Step(inputStepsCount, 100, ch.stepLeftBytes);
+                ch.Disconnect();
+            }
+            else if (input == 3)
+            {
+                Console.WriteLine("O jaki kat obrocic w prawo: ");
+                var inputAngle = int.Parse(Console.ReadLine());
+                var stepsCount = (int)(inputAngle / 7.5f);
+                ch.Step(stepsCount, 100, ch.stepRightBytes);
+                ch.Disconnect();
+            }
+            else if (input == 4)
+            {
+                Console.WriteLine("O jaki kat obrocic w lewo: ");
+                var inputAngle = int.Parse(Console.ReadLine());
+                var stepsCount = (int)(inputAngle / 7.5f);
+                ch.Step(stepsCount, 100, ch.stepLeftBytes);
+                ch.Disconnect();
+            }
+            else if (input == 5)
+            {
+                Console.WriteLine("O ile krokow lewa w dol: ");
+                var inputStepsCount = int.Parse(Console.ReadLine());
+                ch.Step(inputStepsCount, 100, ch.stepLeftDownBytes);
+                ch.Step(1, 100, ch.disconnectBytes);
+
+            }
+            else if (input == 6)
+            {
+                Console.WriteLine("O ile krokow prawa w dol: ");
+                var inputStepsCount = int.Parse(Console.ReadLine());
+                ch.Step(inputStepsCount, 100, ch.stepRightDownBytes);
+                ch.Disconnect();
+            }
+            else if (input == 7)
+            {
+                Console.WriteLine("O jaki kat obrocic lewa w dol: ");
+                var inputAngle = int.Parse(Console.ReadLine());
+                var stepsCount = (int)(inputAngle / 7.5f * 5);
+                ch.Step(stepsCount, 100, ch.stepLeftDownBytes);
+                ch.Disconnect();
+            }
+            else if (input == 8)
+            {
+                Console.WriteLine("O jaki kat obrocic prawa w dol: ");
+                var inputAngle = int.Parse(Console.ReadLine());
+                var stepsCount = (int)(inputAngle / 7.5f * 5);
+                ch.Step(stepsCount, 100, ch.stepRightDownBytes);
+                ch.Disconnect();
+            }
         }
     }
 }
